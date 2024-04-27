73e6730a52eb2a76078e14ce22fc1dd72396522d
Veysi YILDIZ <veysiyildiz@gmail.com>
2024-04-27 07:33:25 +0300

    first commit

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,165 @@
+import { useEffect } from "react";
+import { json } from "@remix-run/node";
+import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($input: ProductInput!) {
+        productCreate(input: $input) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+          }
+        }
+      }`,
+    {
+      variables: {
+        input: {
+          title: `${color} Snowboard`,
+        },
+      },
+    },
+  );
+  const responseJson = await response.json();
+  const variantId =
+    responseJson.data.productCreate.product.variants.edges[0].node.id;
+  const variantResponse = await admin.graphql(
+    `#graphql
+      mutation updateVariant($input: ProductVariantInput!) {
+        productVariantUpdate(input: $input) {
+          productVariant {
+            id
+            price
+            barcode
+            createdAt
+          }
+        }
+      }`,
+    {
+      variables: {
+        input: {
+          id: variantId,
+          price: Math.random() * 100,
+        },
+      },
+    },
+  );
+  const variantResponseJson = await variantResponse.json();
+
+  return json({
+    product: responseJson.data.productCreate.product,
+    variant: variantResponseJson.data.productVariantUpdate.productVariant,
+  });
+};
+
+export default function Index() {
+  const nav = useNavigation();
+  const actionData = useActionData();
+  const submit = useSubmit();
+  const isLoading =
+    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
+  const productId = actionData?.product?.id.replace(
+    "gid://shopify/Product/",
+    "",
+  );
+
+  useEffect(() => {
+    if (productId) {
+      shopify.toast.show("Product created");
+    }
+  }, [productId]);
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+
+  return (
+    <Page>
+      <BlockStack gap="500">
+        <Layout>
+          <Layout.Section>
+            <Card>
+              <BlockStack gap="500">
+                <BlockStack gap="200">
+                  <Text as="h2" variant="headingMd">
+                    My Very First Shopify app Image Banner 🎉
+                  </Text>
+                </BlockStack>
+                <BlockStack gap="200">
+                  <Text>
+                    This is a simple example of a Shopify app built with Remix.
+                    Created using Shopify CLI.
+                  </Text>
+                  <Text>
+                    Simply click the Link Online Store button to view list of
+                    sublinks. Go to Themes Page. And click on the Customize
+                    button to configure your page and include the Image Banner
+                    extension.
+                  </Text>
+                </BlockStack>
+                <BlockStack gap="200">
+                  <img
+                    src="/assets/add_section.png"
+                    alt="add_section"
+                    width={600}
+                  />
+                </BlockStack>
+                <BlockStack gap="200">
+                  <Text>
+                    Upload your images and select the product you want to show.
+                  </Text>
+                </BlockStack>
+                <BlockStack gap="200">
+                  <img
+                    src="/assets/configure_section.png"
+                    alt="configure_section"
+                    width={600}
+                  />
+                </BlockStack>
+                <BlockStack gap="200">
+                  <Text>
+                    Save your changes and view your online store to see it in
+                    action.
+                  </Text>
+                </BlockStack>
+              </BlockStack>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
---
+++ extensions/theme-extension-image-banner/assets/javascript.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1.4,
+    centeredSlides: false,
+    grabCursor: true,
+    spaceBetween: 24,
+  });
+});
